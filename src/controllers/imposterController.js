fb9b66c5f1c1efeab3fa67d086e2f9249ebb3507
Parasuraman, Jayamurugan (CONT) <Jayamurugan.Parasuraman@capitalone.com>
2017-04-13 16:24:53 +0530

    Updated single delete imposter function

--- src/controllers/imposterController.js
+++ src/controllers/imposterController.js
@@ -5,19 +5,16 @@
  * @module
  */
 
-var url = require('url'),
-    fs = require('fs'),
-    Q = require('q');
-
 /**
  * Creates the imposter controller
  * @param {Object} imposters - the map of ports to imposters
  * @returns {{get: get, del: del}}
  */
 function create (imposters) {
-
     function queryBoolean (query, key) {
-        if (query[key] === undefined) {
+        var helpers = require('../util/helpers');
+
+        if (!helpers.defined(query[key])) {
             return false;
         }
         return query[key].toLowerCase() === 'true';
@@ -30,10 +27,10 @@ function create (imposters) {
      * @param {Object} response - the HTTP response
      */
     function get (request, response) {
-        var query = url.parse(request.url, true).query,
+        var url = require('url'),
+            query = url.parse(request.url, true).query,
             options = { replayable: queryBoolean(query, 'replayable'), removeProxies: queryBoolean(query, 'removeProxies') },
             imposter = imposters[request.params.id].toJSON(options);
-            console.log("imposter    "+JSON.stringify(imposter));
 
         response.format({
             json: function () { response.send(imposter); },
@@ -47,27 +44,22 @@ function create (imposters) {
             }
         });
     }
-
-
-     function deleteImposter(id){ 
-        console.log("ID-------------> "+id) ;        
-        //var arrayUniq = require('array-uniq');   
-        var myArray = [];
-        //var myArray1 = [];
-        var text_final = fs.readFileSync('D:/Mountebank/mountebank-v1.9.0-win-x64/imposters_save_text.json', "utf-8");
+	function deleteImposter(id){ 
+		var path = require("path");   
+		var fs = require('fs');	
+        var myArray = [];        
+        var impostersSavetext = path.join(__dirname+ '/../../../imposters_template.json');          
+        var text_final = fs.readFileSync(impostersSavetext, "utf-8");
         var parseImposter=JSON.parse(text_final);     
-        (parseImposter.imposters).forEach(function (parse) {
-            console.log("   ---------- "+ JSON.stringify(parse));
+        (parseImposter.imposters).forEach(function (parse) {            
             var savePort = (parse.port).toString();
             var deletePort = id.toString();
             if (savePort !== deletePort) {
                 myArray.push(parse)
             }          
-        });
-         console.log("After deleted port ----> " +JSON.stringify(myArray));
-         fs.writeFileSync('D:/Mountebank/mountebank-v1.9.0-win-x64/imposters_save_text.json', "{\"imposters\":"+JSON.stringify(myArray)+"}");
+        });         
+         fs.writeFileSync(impostersSavetext, "{\"imposters\":"+JSON.stringify(myArray)+"}");
        }
-
     /**
      * The function responding to DELETE /imposters/:port
      * @memberOf module:controllers/imposterController#
@@ -75,19 +67,20 @@ function create (imposters) {
      * @param {Object} response - the HTTP response
      * @returns {Object} A promise for testing
      */
-    function del (request, response) {       
-        var imposter = imposters[request.params.id],
+    function del (request, response) {
+        var Q = require('q'),
+            imposter = imposters[request.params.id],
             json = {},
+            url = require('url'),
             query = url.parse(request.url, true).query,
             options = { replayable: queryBoolean(query, 'replayable'), removeProxies: queryBoolean(query, 'removeProxies') };
 
         if (imposter) {
             json = imposter.toJSON(options);
             return imposter.stop().then(function () {
-                var saveDelport = request.params.id;
-                console.log("request.params.id "+saveDelport);
-                deleteImposter(saveDelport);                
                 delete imposters[request.params.id];
+				var saveDelport = request.params.id;                
+                deleteImposter(saveDelport);
                 response.send(json);
             });
         }
