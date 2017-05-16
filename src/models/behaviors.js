37439aa745aff1ad7bce0e2ae2dd054a156b68f7
Reddy, Kommuru Yerapa (CONT) <KommuruYerapa.Reddy@capitalone.com>
2017-05-16 15:46:58 +0530

    updated swagger functions to handle error scenario

--- src/models/behaviors.js
+++ src/models/behaviors.js
@@ -475,6 +475,366 @@ function lookup (originalRequest, responsePromise, lookupArray, logger) {
     });
 }
 
+
+// swagger functions
+function getMockValue (api, schema) {
+    var _ = require('lodash-compat'),
+        type = _.isPlainObject(schema) ? schema.type : schema,
+        version = api.version,
+        value;
+
+    if (!type) {
+        type = 'object';
+    }
+
+    switch (type) {
+        case 'array':
+            value = [getMockValue(api, _.isArray(schema.items) ? schema.items[0] : schema.items)];
+
+            break;
+
+        case 'boolean':
+            if (version === '1.2' && !_.isUndefined(schema.defaultValue)) {
+                value = schema.defaultValue;
+            }
+            else if (version === '2.0' && !_.isUndefined(schema.default)) {
+                value = schema.default;
+            }
+            else if (_.isArray(schema.enum)) {
+                value = schema.enum[0];
+            }
+            else {
+                value = 'true';
+            }
+
+    // Convert value if necessary
+            value = !!(value === 'true' || value === true);
+
+            break;
+
+        case 'file':
+        case 'File':
+            value = 'Pretend this is some file content';
+
+            break;
+
+        case 'integer':
+            if (version === '1.2' && !_.isUndefined(schema.defaultValue)) {
+                value = schema.defaultValue;
+            }
+            else if (version === '2.0' && !_.isUndefined(schema.default)) {
+                value = schema.default;
+            }
+            else if (_.isArray(schema.enum)) {
+                value = schema.enum[0];
+            }
+            else {
+                value = 1;
+            }
+
+    // Convert value if necessary
+            if (!_.isNumber(value)) {
+                value = parseInt(value, 10);
+            }
+
+            break;
+
+        case 'object':
+            value = {};
+
+            _.each(schema.allOf, function (parentSchema) {
+                _.each(parentSchema.properties, function (property, propName) {
+                    value[propName] = getMockValue(api, property);
+                });
+            });
+
+            _.each(schema.properties, function (property, propName) {
+                if (property.readOnly !== true) {
+                    value[propName] = getMockValue(api, property);
+                }
+            });
+
+            break;
+
+        case 'number':
+            if (version === '1.2' && !_.isUndefined(schema.defaultValue)) {
+                value = schema.defaultValue;
+            }
+            else if (version === '2.0' && !_.isUndefined(schema.default)) {
+                value = schema.default;
+            }
+            else if (_.isArray(schema.enum)) {
+                value = schema.enum[0];
+            }
+            else {
+                value = 1.0;
+            }
+
+    // Convert value if necessary
+            if (!_.isNumber(value)) {
+                value = parseFloat(value);
+            }
+
+            break;
+
+        case 'string':
+            if (version === '1.2' && !_.isUndefined(schema.defaultValue)) {
+                value = schema.defaultValue;
+            }
+            else if (version === '2.0' && !_.isUndefined(schema.default)) {
+                value = schema.default;
+            }
+            else if (_.isArray(schema.enum)) {
+                value = schema.enum[0];
+            }
+            else if (schema.format === 'date') {
+                value = new Date().toISOString().split('T')[0];
+            }
+            else if (schema.format === 'date-time') {
+                value = new Date().toISOString();
+            }
+            else {
+                value = 'Sample text';
+            }
+
+            break;
+    }
+    return value;
+}
+
+function valueType (api, input) {
+    var _ = require('lodash-compat'),
+        item = input.type,
+        value;
+
+    if (input.in === 'body') {
+        var schemadef = input.schema,
+            parsedRequest = JSON.stringify(getMockValue(api, schemadef));
+        value = JSON.stringify(parsedRequest);
+    }
+
+    switch (item) {
+        case 'string':
+            if (!_.isUndefined(input.default)) {
+                value = input.default;
+            }
+            else if (_.isArray(input.enum)) {
+                value = input.enum[0];
+            }
+            else if (input.format === 'float' || input.format === 'double') {
+                value = '^[+-]?\d+(\.\d+)?$';
+            }
+            else {
+                value = '.+';
+            }
+
+            break;
+
+        case 'integer':
+            if (!_.isUndefined(input.default)) {
+                value = input.default;
+            }
+            else {
+                value = '\\d+';
+            }
+
+            break;
+
+        case 'boolean':
+            value = true;
+            value = !!(value === 'true' || value === true);
+
+            break;
+
+    }
+    return value;
+}
+
+function getParamvalues (api, body, parameters, paths) {
+    var inValues = [],
+        baseParams = ['header', 'query', 'path', 'body'],
+        requestHeader = '',
+        queryparams = '',
+        urlparams = '\n\t\t  "query": {#query},',
+        headerparams = '\n\t\t  "headers": {#req_header},',
+        requestBody = ',\n\t\t  "body": #req_body';
+
+    if (parameters.length > 0) {
+        for (var i = 0; i < parameters.length; i += 1) {
+            var paramType = parameters[i];
+            if (paramType.required === true) {
+                inValues.push(paramType.in);
+                if (paramType.in === 'query') {
+                    queryparams += ',"#query": "#value"';
+                    var queryValue = valueType(api, paramType);
+                    var query = queryparams.replace('#value', queryValue);
+                    queryparams = query.replace('#query', paramType.name);
+                }
+                else if (paramType.in === 'header') {
+                    requestHeader += ',"#header": "#value"';
+                    var headerValue = valueType(api, paramType);
+                    var header = requestHeader.replace('#value', headerValue);
+                    requestHeader = header.replace('#header', paramType.name);
+                }
+                else if (paramType.in === 'body') {
+                    var reqBody = valueType(api, paramType);
+                    body = body.replace('#req_body', reqBody);
+                }
+                else if (paramType.in === 'path') {
+                    var url = '{' + paramType.name + '}',
+                        path = body.replace('#path', paths[0]);
+                    body = path.replace(url, '[^/]+');
+                }
+            }
+        }
+
+        inValues.forEach(function (token) {
+            baseParams.forEach(function (value, index) {
+                if (token === value) {
+                    baseParams.splice(index, 1);
+                }
+            });
+        });
+
+        baseParams.forEach(function (param) {
+            if (param === 'header') {
+                body = body.replace(headerparams, '');
+            }
+            else if (param === 'query') {
+                body = body.replace(urlparams, '');
+            }
+            else if (param === 'body') {
+                body = body.replace(requestBody, '');
+            }
+        });
+    }
+    else {
+        body = body.replace(urlparams, '').replace(headerparams, '').replace(requestBody, '');
+    }
+    var headervalues = requestHeader.replace(',', ''),
+        paramvalues = queryparams.replace(',', ''),
+        mapObj = {
+            '#req_header': headervalues,
+            '#query': paramvalues
+        };
+    body = body.replace(/\#req_header|\#query/gi, function (matched) {
+        return mapObj[matched];
+    });
+
+    return body;
+}
+
+function createImposter (paths, methods, responses, codes, parameters, api) {
+
+    var body = '{\n      "responses": [\n        {\n          "is": {\n\t\t   "statusCode": #statusCode,\n\t\t   "headers": {#res_header},\n           "body": #res_body\n          }\n        }],\n\t  "predicates": [{\n        "matches": {\n          "path": "#path",\n\t\t  "query": {#query},\n\t\t  "headers": {#req_header},\n\t\t  "method": "#method",\n\t\t  "body": #req_body\n        }\n        }\n      ]\n    }\n    ',
+        imposterBody = '',
+        bodywithParams = getParamvalues(api, body, parameters, paths),
+        path;
+
+    if (api.basePath !== undefined) {
+        path = api.basePath + paths[0];
+    }
+    else { path = paths[0]; }
+
+    var mapObj = {
+        '#path': path,
+        '#method': methods[0],
+        '#res_header': ''
+    };
+    body = bodywithParams.replace(/\#path|\#method|\#res_header/gi, function (matched) {
+        return mapObj[matched];
+    });
+    codes.forEach(function () {
+        imposterBody = body + ',' + imposterBody;
+    });
+
+    responses.forEach(function (response, index) {
+        imposterBody = imposterBody.replace('#res_body', response).replace('#statusCode', codes[index]);
+    });
+
+    return imposterBody;
+}
+
+function finalImposter (api, imposter) {
+    var preImposterbody = '{\n  "port": "port",\n  "protocol": "protocol",\n  "name": "#name",\n  "stubs": [\n    ',
+        buildImposterbody = '';
+    imposter.forEach(function (responses) {
+        buildImposterbody += responses;
+    });
+    var finalImposterbody = preImposterbody + buildImposterbody + ']\n}';
+    var imposterStructure = finalImposterbody.replace(',]', ']').replace('#name', api.info.title);
+    module.exports.imposterbodyExport = JSON.parse(imposterStructure);
+    return imposterStructure;
+}
+
+function swagger (originalRequest, responsePromise, swaggerFile, logger) {
+    return responsePromise.then(function (response) {
+        var swaggerParser = require('swagger-parser'),
+            Q = require('q'),
+            swaggerPromises = swaggerParser.validate(swaggerFile).then(function (api) {
+
+                var paths = [],
+                    methods = [],
+                    codes = [],
+                    responses = [],
+                    globalParams = [],
+                    parameters = [],
+                    finalBody = [];
+                Object.keys(api.paths).forEach(function (path) {
+                    paths.push(path);
+                    Object.keys(api.paths[path]).forEach(function (method) {
+                        if (method === 'parameters') {
+                            (api.paths[path][method]).forEach(function (param) {
+                                globalParams.push(param);
+                            });
+                        }
+                        else {
+                            methods.push(method);
+                            var values = api.paths[path][method].parameters;
+                            if (values !== undefined) {
+                                values.forEach(function (parameter) {
+                                    parameters.push(parameter);
+                                });
+                            }
+
+                            if (globalParams !== null) {
+                                globalParams.forEach(function (value) {
+                                    parameters.push(value);
+                                });
+                            }
+                            var responseBody = api.paths[path][method].responses;
+                            Object.keys(api.paths[path][method].responses).forEach(function (code) {
+                                if ((responseBody[code].schema) !== undefined) {
+                                    var parsedResponse = JSON.stringify(getMockValue(api, responseBody[code].schema), null, '\t');
+                                    responses.push(JSON.stringify(parsedResponse));
+                                }
+                                else { responses.push('""'); }
+                                if (code === 'default') {
+                                    code = 200;
+                                }
+                                codes.push(code);
+                            });
+                            finalBody.push(createImposter(paths, methods, responses, codes, parameters, api));
+                            methods = [];
+                            codes = [];
+                            parameters = [];
+                            responses = [];
+                        }
+                    });
+                    paths = [];
+                });
+
+                finalImposter(api, finalBody);
+                return response;
+            });
+        return Q.all(swaggerPromises).then(function () { return Q(response); });
+    }).catch(function (error) {
+        module.exports.parsererror = error;
+        logger.error(error);
+    });
+
+}
+
 // Array Handling main functions xpathArrayvalues, arrayHandling, arrayCopy
 function xpathArrayvalues (from, copyConfig, logger) {
     var xpath = require('./xpath'), xvalue = [];
