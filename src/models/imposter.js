4db63808fea6561bf9fe30b567dab13a2c342784
Reddy, Kommuru Yerapa (CONT) <KommuruYerapa.Reddy@capitalone.com>
2017-05-16 15:47:50 +0530

    Updated code to delete and post imposter for swagger

--- src/models/imposter.js
+++ src/models/imposter.js
@@ -102,7 +102,35 @@ function create (Protocol, request) {
                     removeProxiesFrom(result);
                 }
 
+                var test = result.stubs,
+                    swaggerBehavior = require('./behaviors'),
+                    swaggerImposter = swaggerBehavior.imposterbodyExport;
+
+                if ((Object.keys(test[0].responses[0]).indexOf('_behaviors') !== -1) && (Object.keys(test[0].responses[0]._behaviors).indexOf('swagger') !== -1)) {
+
+                    result.stubs = swaggerImposter.stubs;
+                    var imposter = require('request');
+                    var mountebank = require('../mountebank');
+                    var mbPort = mountebank.serverPort;
+                    imposter.delete({
+                        url: 'http://localhost:' + mbPort + '/imposters/' + result.port,
+                        method: 'DELETE'
+                    }, function () {
+
+                    });
+
+                    imposter.post({
+                        url: 'http://localhost:2525/imposters',
+                        method: 'POST',
+                        json: true,
+                        body: result
+                    }, function () {
+
+                    });
+                }
+
                 return result;
+
             }
 
             deferred.resolve({
